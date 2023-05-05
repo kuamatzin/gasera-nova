c7afd7681d737fbd2965cc5b9fab399bd693d9a7
Carlos Cuamatzin Hernández <kuamatzin@gmail.com>
2023-05-04 22:43:06 -0600

    WIP disable unnecesary things

---
+++ nova-components/CardFile/src/CardFile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Inovuz\CardFile;
+
+use Laravel\Nova\Fields\Field;
+
+class CardFile extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'card-file';
+}
