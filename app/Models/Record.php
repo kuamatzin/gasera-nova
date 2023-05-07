60b54c147a908b8f339b3c65e8a47d19243f4d7c
Carlos Cuamatzin Hernández <kuamatzin@gmail.com>
2023-05-07 07:30:41 -0600

    Set documentation status

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -22,4 +22,19 @@ class Record extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @param $value
+     * @return false|string
+     */
+    protected function setDocumentacionAttribute($documentation): bool|string
+    {
+        $status_values = array_filter($documentation, fn ($value, $key) => strpos($key, '_status', 0), ARRAY_FILTER_USE_BOTH);
+        $status_values = array_map(fn ($value) => $value === null ? 'revision' : $value, $status_values);
+        foreach ($status_values as $key => $value) {
+            $documentation[$key] = $value;
+        }
+
+        return $this->attributes['documentacion'] = json_encode($documentation);
+    }
 }
